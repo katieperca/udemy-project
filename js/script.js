bf4c425ab43fc049c1f75dc8cb22015b25beaac9
katieperca <katieperca@gmail.com>
2020-08-26 18:48:11 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);
+
+const user = 'Ivan';
+let users = ['Ivan', 'Maria', 'Katya', 'Oleg'];
+
+console.log(`Привет, ${users[2]}`);
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+
